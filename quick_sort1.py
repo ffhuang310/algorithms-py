e9124c9713953aa6da8dd5cd3911302946b5fe01
feifan <ffhuang07@gmail.com>
2018-11-29 21:46:46 +1100

    quick sort py code example in quick_sort.py quick sort  practice example in quick_sort1.py

---
+++ quick_sort1.py
@@ -0,0 +1,16 @@
+def quick_sort1(list):
+    quick_sort2(list,0,len(list)-1)
+
+def quick_sort2(list,lo,hi):
+
+
+
+
+
+
+
+    # pivot = len(list) - 1
+    # left = 0
+    # right = pivot - 1
+    # if list(left) < pivot:
+    #     left = left + 1
